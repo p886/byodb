01737d13ccfa479807587a749d6562ea83f87f14
Philipp Antar <philippantar@gmail.com>
2019-11-28 18:26:31 +0100

    Add set command

--- cmd/set.go
+++ cmd/set.go
@@ -6,19 +6,20 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/p886/byodb/retrieval"
+	"github.com/p886/byodb/commandparser"
+	"github.com/p886/byodb/storage"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(setCmd)
 }
 
-var getCmd = &cobra.Command{
-	Use:   "get [key]",
-	Short: "Get data",
-	Long:  `Get data associated with a key`,
-	Args:  cobra.ExactValidArgs(1),
+var setCmd = &cobra.Command{
+	Use:   "set [key] [value]",
+	Short: "Set data",
+	Long:  `Persist value associated with key to the database`,
+	Args:  cobra.ExactValidArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		err := godotenv.Load(envFile)
 		if err != nil {
@@ -26,11 +27,12 @@ var getCmd = &cobra.Command{
 		}
 		storageFilePath := os.Getenv("STORAGE_FILE_PATH")
 		key := args[0]
-		result, err := retrieval.Retrieve(storageFilePath, key)
+		value := args[1]
+		storage.Store(storageFilePath, commandparser.Query{Command: "SET", Key: key, Value: value})
 		if err != nil {
 			fmt.Printf("Error getting data with key '%s': %s\n", key, err.Error())
 			os.Exit(1)
 		}
-		fmt.Println(string(result))
+		fmt.Printf("Successfully saved key: '%s', value: '%s'\n", key, value)
 	},
 }
